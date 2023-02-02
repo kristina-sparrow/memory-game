631e9528f5dc51c43457c855ac3e289f65fd9042
kristina-sparrow <118679841+kristina-sparrow@users.noreply.github.com>
2023-02-02 15:01:07 -0700

    Add initial game logic

---
+++ src/app/components/Game.js
@@ -0,0 +1,95 @@
+import React, { useState, useEffect } from "react";
+import CardGrid from "./CardGrid";
+import authorList from "../data/authors";
+
+export default function Game() {
+  const [level, setLevel] = useState(1);
+  const [authors, setAuthors] = useState([]);
+  const [clickedCards, setClickedCards] = useState([]);
+  const [score, setScore] = useState(0);
+  const [highScore, setHighScore] = useState(0);
+
+  function calculateRequiredCards(level) {
+    switch (level) {
+      case 1:
+        return 4;
+      case 2:
+        return 6;
+      case 3:
+        return 8;
+      case 4:
+        return 10;
+      case 5:
+        return 12;
+      default:
+        return 12;
+    }
+  }
+
+  useEffect(() => {
+    function loadCards() {
+      const amount = calculateRequiredCards(level);
+      setAuthors(shuffleCards(fetchAuthors(amount)));
+    }
+    loadCards();
+  }, [level]);
+
+  function fetchAuthors(amount) {
+    const authors = [];
+    for (let i = 0; i < amount; i++) {
+      const randomIndex = Math.floor(Math.random() * 12);
+      const author = authorList[randomIndex];
+      const name = author.name;
+      const image = author.imageURL;
+      authors.push({ name, image });
+    }
+    return authors;
+  }
+
+  function shuffleCards(array) {
+    return [...array].sort(() => Math.random() - 0.5);
+  }
+
+  function handleCardClick(e) {
+    const card = e.target.parentNode.lastChild.textContent;
+    playSelection(card);
+    setAuthors(shuffleCards(authors));
+  }
+
+  function playSelection(card, level) {
+    if (!clickedCards) {
+      updateScore();
+      setClickedCards([card]);
+      return;
+    }
+    if (clickedCards.includes(card)) {
+      if (score > highScore) {
+        setHighScore(score);
+      }
+      toggleGameOver();
+      return;
+    }
+    setClickedCards([...clickedCards, card]);
+    updateScore();
+    if (clickedCards.length === calculateRequiredCards(level) - 1) {
+      setClickedCards([]);
+      setLevel(level + 1);
+    }
+  }
+
+  function updateScore() {
+    const newScore = score + 1;
+    if (newScore > highScore) setHighScore(newScore);
+    setScore(newScore);
+  }
+
+  function toggleGameOver() {
+    //do something...
+  }
+
+  return (
+    <div className="game">
+      <CardGrid authors={authors} onClick={handleCardClick} />
+    </div>
+  );
+}
