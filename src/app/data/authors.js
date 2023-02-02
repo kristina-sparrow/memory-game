0c143491e28e71d62290bd9c84140ff84d456cfb
kristina-sparrow <118679841+kristina-sparrow@users.noreply.github.com>
2023-02-02 13:13:33 -0700

    Add author list and photos

---
+++ src/app/data/authors.js
@@ -0,0 +1,16 @@
+const authorList = [
+  { name: "Edgar Allan Poe", imageURL: "../assets/eap.jpg" },
+  { name: "Hermann Hesse", imageURL: "../assets/hh.jpeg" },
+  { name: "Franz Kafka", imageURL: "../assets/fj.jpeg" },
+  { name: "Leo Tolstoy", imageURL: "../assets/lt.jpeg" },
+  { name: "Fyodor Dostoyevsky", imageURL: "../assets/fd.jpeg" },
+  { name: "J.R.R. Tolkien", imageURL: "../assets/jrt.jpg" },
+  { name: "Aldous Huxley", imageURL: "../assets/ah.png" },
+  { name: "George Orwell", imageURL: "../assets/go.jpeg" },
+  { name: "Ralph Waldo Emerson", imageURL: "../assets/rwe.jpeg" },
+  { name: "Mary Shelley", imageURL: "../assets/ms.jpg" },
+  { name: "Virginia Woolf", imageURL: "../assets/vw.jpeg" },
+  { name: "Jane Austen", imageURL: "../assets/ja.jpeg" },
+];
+
+export default authorList;
